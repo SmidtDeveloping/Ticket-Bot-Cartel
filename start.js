92a0845c3daa68df2ad64881668dcf9e70b021ee
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2025-01-23 02:47:45 +0100

    Create start.js

---
+++ start.js
@@ -0,0 +1,7 @@
+const { startPremiumBot, stopPremiumBot } = require('./pm2');
+require("dotenv").config()
+const token1 = process.env.TOKEN1
+const token2 = process.env.TOKEN2
+// Start een bot met userId en token
+startPremiumBot('12345', token1);
+startPremiumBot("", token2)
