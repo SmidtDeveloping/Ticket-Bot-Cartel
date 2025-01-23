49f97ff7583021735cacb1aece315062b9a7f73b
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2025-01-23 02:52:04 +0100

    Update start.js

--- start.js
+++ start.js
@@ -4,4 +4,4 @@ const token1 = process.env.TOKEN1
 const token2 = process.env.TOKEN2
 // Start een bot met userId en token
 startPremiumBot('1272523687513292929', token1);
-startPremiumBot("", token2)
+startPremiumBot("1331803190252802109", token2)
